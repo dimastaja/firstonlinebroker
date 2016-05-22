3bf4cc8ec02f2a9eff729fc3042878a360dcc722
dimastaja <dmitriy.supov@gmail.com>
2016-05-23 01:05:37 +0300

    templates

---
+++ wp-content/themes/fob/oneproduct.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The template for displaying product.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ * Template Name: Один Продукт
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package fob
+ */
+
+//get_header(); ?>
+
+
+<?
+
+	the_post();
+?>
+        <!-- Section: ins property-->
+
+<section id="insPorpery" class="intro section ins-section" style="background: rgba(0, 0, 0, 0.03) none repeat scroll 0 0;">
+<div class="ins-wrapper">
+<div class="prod_wrapper">
+<div class="section-heading ">
+<h3>Страхование имущества</h3>
+
+</div>
+<div class="intro-block no-left-margin">
+<h4 class="intro-block-title">Что это?</h4>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+</div>
+<div class="intro-block">
+<h4 class="intro-block-title">Зачем это?</h4>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+</div>
+<div class="intro-block no-left-margin">
+<h4 class="intro-block-title">Риски?</h4>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+</div>
+<div class="intro-block">
+<h4 class="intro-block-title">Camo gryadeshi?</h4>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+</div>
+<div class="clear"></div>
+</div>
+<div class="right-button-wrapper"><button id="" class="btn btn-skin pull-right" data-toggle="modal" data-target="#ins_rules">Правила
+страхование</button>
+<button id="" class="btn btn-skin pull-right" data-toggle="modal" data-target="#ins_ex_practice">Примеры из
+практики</button>
+<button id="" class="btn btn-skin pull-right" data-toggle="modal" data-target="#ins_faq">FAQ</button>
+<button id="" class="btn btn-skin pull-right btn-order" data-href="/order.php">Заказать</button></div>
+<div class="clear"></div>
+</div>
+</section><!-- /Section: ins property -->
+
+
+
+<?php
+
+//get_footer();
--- wp-content/themes/fob/simpletext.php
+++ wp-content/themes/fob/simpletext.php
@@ -1,1 +1,1 @@
-<? echo 'asdfas';?>
+s<? echo 'asdfas';?>
